74b97aa33feb3320c41a2bb73e15ae0b8de56ea1
Ethan Halprin <ethan.halprin@gmail.com>
2020-12-20 16:10:25 +0200

    Startup project + gitignore

---
+++ FitV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitV
+//
+//  Created by Ethan on 20/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
